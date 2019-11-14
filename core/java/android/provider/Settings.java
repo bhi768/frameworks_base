68b7421d39d8f428b3b4223c939ed7992e1a1097
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 14:07:44 +0530

    base: Add refresh rate setting [1/2]
    Change-Id: Ie794574f624bf48f58876ac6761ce28dccf0912c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4856,6 +4856,11 @@ public final class Settings {
         public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
